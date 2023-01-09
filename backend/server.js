5ef303315bb711a79305653d7e496b9e348a16e5
Malhar Kajale <malharkajale@gmail.com>
2023-01-09 19:32:14 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-
 const mongoose = require('mongoose')
+const path = require('path')
 
 const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
@@ -12,9 +12,15 @@ const port = process.env.PORT || 4000
 
 app.use(express.json())
 
+app.use(express.static(path.resolve(__dirname, 'build')));
+
 app.use('/api/user', userRoutes)
 
 app.use('/api/product', productRoutes)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGO_URI)
